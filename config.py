59448cbaa827dc1ae4689b7b8d69ad6fb107b8b9
Raqawi <134682323+Abnsyria03@users.noreply.github.com>
2023-07-30 16:35:00 +0300

    Add files via upload

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 class Config:
-    API_ID = 
-    API_HASH = ""
-    TOKEN = ""  
-    START_PIC = "" 
-    CHAT = ""    
+    API_ID = 19312827
+    API_HASH = "84da7f08e87849853b2fa6728e4192a2"
+    TOKEN = "6225367412:AAF_stl_eJUgacMubl8D7C4mFUf9LVxEL_g"  
+    START_PIC = "start" 
+    CHAT = "https://t.me/GnuuG"    
