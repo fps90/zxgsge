efd5b6fc80d8af5761122d95e3a51a0bc2443c47
Raqawi <134682323+Abnsyria03@users.noreply.github.com>
2023-07-30 18:24:51 +0300

    Add files via upload

--- config.py
+++ config.py
@@ -2,5 +2,5 @@ class Config:
     API_ID = 19312827
     API_HASH = "84da7f08e87849853b2fa6728e4192a2"
     TOKEN = "6225367412:AAF_stl_eJUgacMubl8D7C4mFUf9LVxEL_g"  
-    START_PIC = "start" 
-    CHAT = "https://t.me/GnuuG"    
+    START_PIC = "https://telegra.ph/file/f48c384b02ebfa4f488e9.jpg" 
+    CHAT = "-1001840864904"    
