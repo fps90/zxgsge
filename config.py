7c7ccdc5eb80d703ff9a6dbf244cf2c607febdab
علش <153824248+nesrx@users.noreply.github.com>
2024-01-01 04:16:57 +0300

    Update config.py

--- config.py
+++ config.py
@@ -2,5 +2,5 @@ class Config:
     API_ID = 19312827
     API_HASH = "84da7f08e87849853b2fa6728e4192a2"
     TOKEN = "6347683888:AAEF5TlwNHYFX0hJqElrDfiBoD8ZWieF3BQ"  
-    START_PIC = "https://telegra.ph/file/e4055f1c03998748f2d55.jpg" 
+    START_PIC = "https://telegra.ph/file/30849734c0436f1960e6a.jpg" 
     CHAT = "-1002006583271"    
