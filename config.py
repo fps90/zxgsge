bff8030a49f0d8fe790753c2f9cae279b95ae811
Team Freedom <167733685+fps90@users.noreply.github.com>
2024-08-06 21:38:59 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 class Config:
     API_ID = 19312827
     API_HASH = "84da7f08e87849853b2fa6728e4192a2"
-    TOKEN = "6347683888:AAEF5TlwNHYFX0hJqElrDfiBoD8ZWieF3BQ"  
+    TOKEN = "5249489492:AAFs1nvsBvnI8GCS_Km-EG_yXwtdIrcRdGo"  
     START_PIC = "https://telegra.ph/file/30849734c0436f1960e6a.jpg" 
-    CHAT = "-1002006583271"    
+    CHAT = "-1002136390368"    
